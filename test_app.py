9cf22246f18d2d14cbf134b6ca949c9bf13acc72
All Fiction <126734865+ziqkimi308@users.noreply.github.com>
2026-07-01 17:18:28 +0800

    Flask startup app and cicd yml

---
+++ test_app.py
@@ -0,0 +1,22 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_returns_200(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+def test_home_content(client):
+    response = client.get("/")
+    assert b"Hello from my CI/CD Pipeline!" in response.data
+
+def test_health_returns_ok(client):
+    response = client.get("/health")
+    assert response.status_code == 200
+    json_data = response.get_json()
+    assert json_data["status"] == "ok"
