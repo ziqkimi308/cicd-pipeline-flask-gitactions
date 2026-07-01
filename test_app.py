c542c08f44bbdc0573d9aa444b2546169ac0bf9d
All Fiction <126734865+ziqkimi308@users.noreply.github.com>
2026-07-01 18:00:22 +0800

    fix space formatting

--- test_app.py
+++ test_app.py
@@ -7,16 +7,20 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_home_returns_200(client):
     response = client.get("/")
     assert response.status_code == 200
 
+
 def test_home_content(client):
     response = client.get("/")
     assert b"Hello from my CI/CD Pipeline!" in response.data
+
 
 def test_health_returns_ok(client):
     response = client.get("/health")
     assert response.status_code == 200
     json_data = response.get_json()
     assert json_data["status"] == "ok"
+	
